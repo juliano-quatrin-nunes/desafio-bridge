ce2fcd96fdc5e005e39dfff06fa821c913464671
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2022-08-08 11:19:42 -0300

    Componentes adicionados e acesso à API do backend para requisitar um número

--- number_web_app/src/main/java/com/desafio_bridge/number_web_app/api/NumberController.java
+++ number_web_app/src/main/java/com/desafio_bridge/number_web_app/api/NumberController.java
@@ -1,6 +1,7 @@
 package com.desafio_bridge.number_web_app.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import com.desafio_bridge.number_web_app.service.NumberService;
 
 @RequestMapping("api/v1/number")
 @RestController
+@CrossOrigin
 public class NumberController {
 
     @Autowired
